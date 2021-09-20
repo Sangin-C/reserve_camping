74434e951781099f5edad83ff79c60e580c1f752
Sangin-C <sangin3435@naver.com>
2021-09-20 22:54:18 +0900

    캠핑 예약 시스템 프로젝트 생성

---
+++ src/main/java/com/locky/camp/reserve_camping/ReserveCampingApplication.java
@@ -0,0 +1,13 @@
+package com.locky.camp.reserve_camping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveCampingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveCampingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/locky/camp/reserve_camping/ReserveCampingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locky.camp.reserve_camping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveCampingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
